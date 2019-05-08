a6636febae95d476e0e2679a95a74022dd8ec6a6
Addi-11 <saddi.5131@gmail.com>
2019-05-09 01:29:05 +0530

    added the post route and body-parser

--- ver1/app.js
+++ ver1/app.js
@@ -1,5 +1,8 @@
 var express =require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine", "ejs");
 
@@ -15,6 +18,12 @@ app.get("/campgrounds",function(req,res){
      ]
 
      res.render("campgrounds",{campgrounds:campgrounds}); //name of data as to be collected: name of data to be passed
+})
+
+app.post("/campgrounds",function(){
+    //add data to the campgrounds array
+    //redirect back to campgrounds page
+
 })
 
 app.listen(8080,'localhost', function(){
